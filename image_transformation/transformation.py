35df56a5e61682195175132cf0c07746bf260ad8
splix <federicosalalois@gmail.com>
2024-06-13 10:36:59 +0200

    Docstrings and error handling improved for cli

--- image_transformation/transformation.py
+++ image_transformation/transformation.py
@@ -1,11 +1,26 @@
 import os
+from typing import Union
 
 import click
 
 from utils.transformation_utils import Transformation
 
 
-def validate_source(ctx, param, value):
+def validate_source(ctx: click.Context, param: click.Parameter, value: str) -> str:
+    """
+    Validate the source parameter.
+
+    Args:
+        ctx (click.Context): The Click context.
+        param (click.Parameter): The Click parameter.
+        value (str): The value of the source parameter.
+
+    Returns:
+        str: The validated source value.
+
+    Raises:
+        click.BadParameter: If the source is not a directory or a .jpg file.
+    """
     if os.path.isdir(value):
         return value
     elif value and not value.lower().endswith('.jpg'):
@@ -13,7 +28,21 @@ def validate_source(ctx, param, value):
     return value
 
 
-def validate_directory(ctx, param, value):
+def validate_directory(ctx: click.Context, param: click.Parameter, value: Union[str, None]) -> Union[str, None]:
+    """
+    Validate the destination directory parameter.
+
+    Args:
+        ctx (click.Context): The Click context.
+        param (click.Parameter): The Click parameter.
+        value (Union[str, None]): The value of the destination directory parameter.
+
+    Returns:
+        Union[str, None]: The validated directory value.
+
+    Raises:
+        click.BadParameter: If the directory cannot be created or is invalid.
+    """
     if value:
         try:
             if not os.path.exists(value):
@@ -21,24 +50,34 @@ def validate_directory(ctx, param, value):
             if not os.path.isdir(value):
                 raise click.BadParameter(f'{value} is not a valid directory.')
         except Exception as e:
-            raise click.BadParameter(f'Could not create directory {value}: {e}')
+            raise click.BadParameter(f"Couldn't create directory {value}: {e}") from e
     return value
 
 
 @click.command()
-@click.option('--src', required=True, help='Source image or directory', callback=validate_source)
-@click.option('--dst', help='Destination directory', callback=validate_directory)
-def transformation(src, dst):
+@click.option(param_decls='--src', required=True, help='Source image or directory', callback=validate_source)
+@click.option(param_decls='--dst', help='Destination directory', callback=validate_directory)
+def transformation(src: str, dst: Union[str, None]) -> None:
+    """
+     Perform the transformation based on the source and destination.
+
+     Args:
+         src (str): The source image or directory.
+         dst (Union[str, None]): The destination directory.
+
+     Raises:
+         click.UsageError: If the source is a directory and the destination is not provided.
+     """
     if os.path.isfile(src):
         click.echo(f'Source file is a valid .jpg image: {src}')
-        transform = Transformation(image_path=src)
+        Transformation(image_path=src)
 
     elif os.path.isdir(src):
         if not dst:
             raise click.UsageError('If --src is a directory, --dst must also be specified and must be a directory.')
         click.echo(f'Source directory: {src}')
         click.echo(f'Destination directory: {dst}')
-        transform = Transformation(input_dir=src, output_dir=dst)
+        Transformation(input_dir=src, output_dir=dst)
     else:
         raise click.UsageError('Invalid source. Must be either a .jpg image or a directory.')
 
